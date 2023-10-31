c4144aedb1d1bc497107ea83f9b618fdbf70247c
mosesedwin1@outlook.com <mosesedwin1@outlook.com>
2023-10-31 23:50:15 +0100

    Started working on the front page of the project

---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import Home from '../pages/Home';
+import Tour from '../pages/Tour';
+import TourDetails from '../pages/TourDetails';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import SearchResultList from '../pages/SearchResultList';
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route exact path='/' element={<Navigate to='/home' />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/tours' element={<Tour />} />
+      <Route path='/tour/:id' element={<TourDetails />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/tour/search' element={<SearchResultList />} />
+    </Routes>
+  )
+}
+
+export default Routers
