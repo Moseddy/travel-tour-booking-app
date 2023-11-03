6b5f88ab6bccaf08c51be5a4656c78bc089b7fac
mosesedwin1@outlook.com <mosesedwin1@outlook.com>
2023-11-03 08:55:12 +0100

    completed the home page design of the travel and tours booking application

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import '../styles/home.css';
 import { Container, Row, Col } from 'reactstrap';
+
 import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from '../assets/images/hero-img02.jpg';
 import heroVideo from '../assets/images/hero-video.mp4';
 import worldImg from '../assets/images/world.png';
+import experienceImg from '../assets/images/experience.png';
+
 import Subtitle from '../shared/Subtitle';
 import SearchBar from '../shared/SearchBar';
 import ServiceList from '../services/ServiceList';
 import FeaturedTourList from '../components/Featured-tours/FeaturedTourList';
+import MasonryImagesGallery from '../components/image-gallery/MasonryImagesGallery';
+import Testimonials from '../components/Testimonial/Testimonials';
+import NewsLetter from '../shared/NewsLetter';
+
 
 const Home = () => {
   return (
@@ -74,6 +81,88 @@ const Home = () => {
         </Row>
       </Container>
     </section>
+    {/* FEATURED TOUR SECTION END */}
+
+    {/* EXPERIENCE SECTION START */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6'>
+              <div className="experience__content">
+                <Subtitle subtitle={'Experience'} />
+
+                <h2>We will serve you<br />with our Experience</h2>
+                <p>Lorem ipsum dolor sit amet consectetur.<br />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Successful Trip</h6>
+                </div>
+
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Regular Clients</h6>
+                </div>
+
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Years Experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg='6'>
+              <div className="experience__img">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
+
+          </Row>
+        </Container>
+      </section>
+    {/* EXPERIENCE SECTION END */}
+
+    {/* GALLERY SECTION START */}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+            <Subtitle subtitle='Gallery' />
+            <h2 className='gallery__title'>Visit our customer <br /> tour gallery</h2>
+          </Col>
+
+          <Col lg='12'>
+            <MasonryImagesGallery />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    {/* GALLERY SECTION END */}
+
+
+    {/* TESTIMONIAL SECTION START */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              <Subtitle subtitle='Fans Love' />
+              <h2 className="testimonial__title">
+                What Our Fans Say About Us
+              </h2>
+            </Col>
+            <Col lg='12'>
+              <Testimonials />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    {/* TESTIMONIAL SECTION END */}
+
+    {/* NEWSLETTER SECTION START */}
+    <NewsLetter />
+    {/* NEWSLETTER SECTION END */}
     </>
   )
 }
--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -2,19 +2,17 @@ import React from 'react'
 import { Card, CardBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './tour-card.css';
+import calculateAvgRating from '../utils/avgRating';
 
 
 
 const TourCard = ({tour}) => {
     
     const { id, title, photo, city, price, featured, reviews } = tour;
-
-    const totalRating = reviews?.reduce((acc, item) => acc + item.rating, 0);
-    const avgRating = 
-    totalRating === 0 ? '' : totalRating === 1 ? totalRating : totalRating/reviews?.length;
+    const { totalRating, avgRating } = calculateAvgRating(reviews);
   return (
     <div className='tour__card'>
-    <Link to={`/tours/${id}`}>
+    <Link to={`/tours/${id}`}> 
         <Card>
             <div className="tour__img">
                 <img src={photo} alt="tour-img" />
